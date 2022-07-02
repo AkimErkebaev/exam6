55d4645fa288788a18ea6f13b9474e19624a33d3
akim <akimmmm@bk.ru>
2022-07-02 14:34:04 +0600

    финишный коммит

--- GuestBook/webapp/admin.py
+++ GuestBook/webapp/admin.py
@@ -5,12 +5,12 @@ from webapp.models import Guest
 
 
 class GuestAdmin(admin.ModelAdmin):
-    list_display = ['id', 'text', 'status', 'created_at', 'author']
+    list_display = ['id', 'text', 'status', 'created_at', 'author', 'updated_at']
     list_display_links = ['author']
     list_filter = ['created_at']
     search_fields = ['created_at']
     fields = ['text', 'status', 'created_at', 'author']
-    readonly_fields = []
+    readonly_fields = ['created_at', 'updated_at']
 
 
 admin.site.register(Guest, GuestAdmin)
