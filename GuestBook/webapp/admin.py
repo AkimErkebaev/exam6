7f1708b5ff369d7421f81fbbf45d42536b7da27f
akim <akimmmm@bk.ru>
2022-07-02 12:11:08 +0600

    настроил панель админа

--- GuestBook/webapp/admin.py
+++ GuestBook/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Guest
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'status', 'created_at', 'author']
+    list_display_links = ['author']
+    list_filter = ['created_at']
+    search_fields = ['created_at']
+    fields = ['text', 'status', 'created_at', 'author']
+    readonly_fields = []
+
+
+admin.site.register(Guest, TaskAdmin)
--- GuestBook/webapp/models.py
+++ GuestBook/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+status_choices = [('active', 'Активно'), ('blocked', 'Заблокировано')]
+
+
+class Guest(models.Model):
+    author = models.CharField(max_length=50, null=False, blank=False, default="NoName", verbose_name="Автор")
+    email = models.EmailField(max_length=50, null=False, blank=False, default="NoEmail", verbose_name="Мэйл")
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name="Текст")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата изменения")
+    status = models.CharField(max_length=50, choices=status_choices, default=status_choices[0][0],
+                              verbose_name="Статус")
+
+    def __str__(self):
+        return f"{self.id}. {self.text}: {self.status} {self.author} {self.email}"
+
+    class Meta:
+        db_table = "guests"
+        verbose_name = "Задание"
+        verbose_name_plural = "Задания"
